36db8355aba278f2caf66dcb64aada1f24597e66
crooba <45910733+crooba@users.noreply.github.com>
2020-01-30 12:21:08 +0900

    Update lstm1.py

--- lstm1.py
+++ lstm1.py
@@ -1,4 +1,8 @@
-# -*- coding: utf-8 -*-
+'''
+학교종이 땡땡땡 악보코드
+g4, g4, a4, a4, g4, g4, e4, g4, g4, e4, e4, d4,  
+g4, g4, a4, a4, g4, g4, e4, g4, e4, d4, e4, c4
+'''
 import keras
 import numpy as np
 from keras.models import Sequential
@@ -8,7 +12,7 @@ from keras.utils import np_utils
 
 np.random.seed(5)
 
-# loss값의 이력을 저장합니다.
+# 손실 이력 클래스 정의
 class LossHistory(keras.callbacks.Callback):
     def init(self):
         self.losses = []
@@ -16,7 +20,7 @@ class LossHistory(keras.callbacks.Callback):
     def on_epoch_end(self, batch, logs={}):
         self.losses.append(logs.get('loss'))
 
-# 데이터를 학습에 맞게 조각내는 함수입니다.
+# 데이터셋 생성 함수
 def seq2dataset(seq, window_size):
     dataset = []
     for i in range(len(seq)-window_size):
@@ -24,36 +28,40 @@ def seq2dataset(seq, window_size):
         dataset.append([code2idx[item] for item in subset])
     return np.array(dataset)        
 
+# 1. 데이터 준비하기
 
+# 코드 사전 정의
 
+code2idx = {'c4':0, 'd4':1, 'e4':2, 'f4':3, 'g4':4, 'a4':5, 'b4':6,
+            'c8':7, 'd8':8, 'e8':9, 'f8':10, 'g8':11, 'a8':12, 'b8':13}
 
+idx2code = {0:'c4', 1:'d4', 2:'e4', 3:'f4', 4:'g4', 5:'a4', 6:'b4',
+            7:'c8', 8:'d8', 9:'e8', 10:'f8', 11:'g8', 12:'a8', 13:'b8'}
 
-code2idx = {'c4':0, 'd4':1, 'e4':2, 'f4':3, 'g4':4, 'a4':5, 'b4':6}
+# 시퀀스 데이터 정의
 
-idx2code = {0:'c4', 1:'d4', 2:'e4', 3:'f4', 4:'g4', 5:'a4', 6:'b4'}
+seq = ['g8', 'e8', 'e4', 'f8', 'd8', 'd4', 'c8', 'd8', 'e8', 'f8', 'g8', 'g8', 'g4',
+       'g8', 'e8', 'e8', 'e8', 'f8', 'd8', 'd4', 'c8', 'e8', 'g8', 'g8', 'e8', 'e8', 'e4',
+       'd8', 'd8', 'd8', 'd8', 'd8', 'e8', 'f4', 'e8', 'e8', 'e8', 'e8', 'e8', 'f8', 'g4',
+       'g8', 'e8', 'e4', 'f8', 'd8', 'd4', 'c8', 'e8', 'g8', 'g8', 'e8', 'e8', 'e4']
 
-
-
-seq = [g4, g4, a4, a4, g4, g4, e4, g4, g4, e4, e4, d4,  
-g4, g4, a4, a4, g4, g4, e4, g4, e4, d4, e4, c4]
-
-# 데이터를 신경망에 맞게 조각냅니다.
+# 2. 데이터셋 생성하기
 
 dataset = seq2dataset(seq, window_size = 4)
 
 print(dataset.shape)
 
-# 입력과 출력(정답)을 나눕니다.
+# 입력(X)과 출력(Y) 변수로 분리하기
 x_train = dataset[:,0:4]
 y_train = dataset[:,4]
 
 max_idx_value = 13
 
-# 입력값을 정규화합니다.
+# 입력값 정규화 시키기
 x_train = x_train / float(max_idx_value)
 
-# 입력을 (샘플 수, 타임스텝, 특성 수)로 형태 변환합니다.
-x_train = np.reshape(x_train, (24, 4, 1))
+# 입력을 (샘플 수, 타임스텝, 특성 수)로 형태 변환
+x_train = np.reshape(x_train, (50, 4, 1))
 
 # 라벨값에 대한 one-hot 인코딩 수행
 y_train = np_utils.to_categorical(y_train)
@@ -62,18 +70,18 @@ one_hot_vec_size = y_train.shape[1]
 
 print("one hot encoding vector size is ", one_hot_vec_size)
 
-# 모델을 설계합니다.
+# 3. 모델 구성하기
 model = Sequential()
 model.add(LSTM(128, batch_input_shape = (1, 4, 1), stateful=True))
 model.add(Dense(one_hot_vec_size, activation='softmax'))
     
-# 모델 학습과정 설정합니다.
+# 4. 모델 학습과정 설정하기
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-# 모델 학습시키기
-num_epochs = 250
+# 5. 모델 학습시키기
+num_epochs = 500
 
-history = LossHistory() #loss값을 저장할 객체를 생성합니다.
+history = LossHistory() # 손실 이력 객체 생성
 
 history.init()
 
@@ -82,8 +90,8 @@ for epoch_idx in range(num_epochs):
     model.fit(x_train, y_train, epochs=1, batch_size=1, verbose=2, shuffle=False, callbacks=[history]) # 50 is X.shape[0]
     model.reset_states()
     
-# 학습과정을 모니터링합니다.
-# %matplotlib inline
+# 6. 학습과정 살펴보기
+%matplotlib inline
 import matplotlib.pyplot as plt
 
 plt.plot(history.losses)
@@ -92,29 +100,29 @@ plt.xlabel('epoch')
 plt.legend(['train'], loc='upper left')
 plt.show()
 
-# 모델의 정확도를 평가합니다.
+# 7. 모델 평가하기
 scores = model.evaluate(x_train, y_train, batch_size=1)
 print("%s: %.2f%%" %(model.metrics_names[1], scores[1]*100))
 model.reset_states()
 
-# 모델을 사용합니다.
+# 8. 모델 사용하기
 
-pred_count = 24 # 최대 예측 개수 정의
+pred_count = 50 # 최대 예측 개수 정의
 
-# 한 스텝을 예측합니다.
+# 한 스텝 예측
 
 seq_out = ['g8', 'e8', 'e4', 'f8']
 pred_out = model.predict(x_train, batch_size=1)
 
 for i in range(pred_count):
-    idx = np.argmax(pred_out[i]) # one-hot 인코딩을 인덱스 값으로 변환합니다.
-    seq_out.append(idx2code[idx]) # seq_out는 최종 악보이므로 인덱스 값을 코드로 변환하여 저장합니다.
+    idx = np.argmax(pred_out[i]) # one-hot 인코딩을 인덱스 값으로 변환
+    seq_out.append(idx2code[idx]) # seq_out는 최종 악보이므로 인덱스 값을 코드로 변환하여 저장
 
 model.reset_states()
     
 print("one step prediction : ", seq_out)
 
-# 곡 전체를 예측합니다
+# 곡 전체 예측
 
 seq_in = ['g8', 'e8', 'e4', 'f8']
 seq_out = seq_in
